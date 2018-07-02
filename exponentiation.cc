ac7af1af90ace021f873efe2bd5fb21a00848ec9
John, Tara <Tara_John@comcast.com>
2018-07-02 14:03:15 -0400

    remove test comment

--- exponentiation.cc
+++ exponentiation.cc
@@ -30,7 +30,6 @@ int open_memory(void);
 void seek_memory(int fd, unsigned long offset);
 
 // Function Definitions
-// adding test
 inline void clflush(volatile void *p, unsigned int size)
 {
    for(unsigned int i=0; i<size; i+=CL_SIZE)
